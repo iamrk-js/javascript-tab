b9ed44593215559f30a0d5cee5fb901c7400c168
iamrk-js <88290009+iamrk-js@users.noreply.github.com>
2023-04-25 20:58:40 +0530

    work on js tab functionlity and UI

---
+++ assets/js/app.js
@@ -0,0 +1,20 @@
+let cl = console.log;
+
+
+const allTabHeads = [...document.querySelectorAll(".myTabHeading li")]
+const tabContentArray = [...document.querySelectorAll(".tabContent")]
+
+const onTabHeadClick = (eve) => {
+    let getId = eve.target.getAttribute('data-id');
+    cl(getId)
+    tabContentArray.forEach(div => div.classList.remove("active"))
+    document.getElementById(getId).classList.add("active");
+    allTabHeads.forEach(li => li.classList.remove("active"));
+    eve.target.classList.add("active")
+}
+
+
+allTabHeads.forEach(li => {
+    // cl(li)
+    li.addEventListener("click", onTabHeadClick)
+})
